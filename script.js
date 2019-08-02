79b06a80362f6b196145b8c99f86ccd885552149
Karan Saini <karansaini282@yahoo.com>
2019-08-02 15:11:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-d3.json('https://bustime.mta.info/api/siri/vehicle-monitoring.json?key=330dbfc1-9009-422d-a5c4-364cf7d082c9&VehicleMonitoringDetailLevel=calls&LineRef=M3').then(data=>
-{
-  console.log(data);
-},error=>
-{
-  console.log(error);
-}
-);
+d3.json('https://bustime.mta.info/api/siri/vehicle-monitoring.json?key=330dbfc1-9009-422d-a5c4-364cf7d082c9&VehicleMonitoringDetailLevel=calls&LineRef=M3')
+  .then(data=>
+    {
+      console.log(data);
+    }
+  );
